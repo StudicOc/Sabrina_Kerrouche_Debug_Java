b96d2921e55f15f125b0c2168705c0c1898f91a8
StudicOC <sabrinaleb48@gmail.com>
2023-07-15 20:59:00 +0200

    File Interface Writer

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataFromFile.java
@@ -0,0 +1,12 @@
+package com.hemebiotech.analytics;
+
+import java.util.Map;
+
+public class WriteSymptomDataFromFile implements ISymptomWriter {
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+
+
+    }
+}
