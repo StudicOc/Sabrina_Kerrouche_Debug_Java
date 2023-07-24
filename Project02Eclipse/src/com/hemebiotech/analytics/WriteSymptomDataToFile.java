c9238b631a330c7e1ba2497d6076734b32c8eed2
StudicOC <sabrinaleb48@gmail.com>
2023-07-24 21:07:49 +0200

    Interface write data

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,2 +1,38 @@
-package com.hemebiotech.analytics;public class WriteSymptomDataToFile {
+package com.hemebiotech.analytics;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WriteSymptomDataToFile implements ISymptomWriter{
+
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+
+
+        Map<String, Integer> writeSymptoms = new HashMap<>();
+
+        try {
+
+            FileWriter fileWriter = new FileWriter("/result.out");
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+
+
+            // Use method write
+
+            writer.close() ;
+
+        } catch (IOException e) {
+            System.out.println("Beurre " + e.getMessage()) ;
+            e.printStackTrace();
+        }
+
+
+
+
+    }
 }
