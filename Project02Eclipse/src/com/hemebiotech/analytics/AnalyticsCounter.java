e9011f018ed570f6f5048e1c6a6b59c753e5af50
StudicOC <sabrinaleb48@gmail.com>
2023-07-28 12:01:56 +0200

    Refactoring AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,13 +1,15 @@
 package com.hemebiotech.analytics;
 
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class AnalyticsCounter {
 
 
 	public static void main(String[] args) throws Exception {
-
 
 		//1-Get symptom
 		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
@@ -17,18 +19,18 @@ public class AnalyticsCounter {
 		//Retrieve result of GetSymptoms
 		TreeMap<String, Integer> counter = countSymptoms(symptoms);
 
+
 		//Boucle and check result with console
 		for (Map.Entry<String, Integer> symptom : counter.entrySet()) {
 			System.out.println(symptom.getKey() + " : " + symptom.getValue() + "\n");
 		}
 
+		writeSymptoms(countSymptoms(symptoms));
+
 	}
 
-	//1-Get symptom
-
+	// 3- Sort symptom
 	public static TreeMap<String, Integer> countSymptoms(List<String> symptoms) {
-
-
 
 		TreeMap<String, Integer> countFrequencySymptom = new TreeMap<>();
 
@@ -50,16 +52,34 @@ public class AnalyticsCounter {
 				//System.out.println("Element Count : "+countFrequencySymptom);
 			}
 		}
-
 		return countFrequencySymptom;
 	}
 
-	// 3- Sort symptom
+
 	// 4 - Write symptom
+	public static void writeSymptoms(Map<String, Integer> symptoms) {
 
+		if (symptoms != null) {
+			try {
+				FileWriter fileWriter = null;
+				fileWriter = new FileWriter("./result.out");
+				BufferedWriter writer = new BufferedWriter(fileWriter);
 
-
-
+				// Parcourir la map: SET : parcourir les clés de la map
+				for (String symptom : symptoms.keySet()) {
+					writer.write(symptom + " : " + symptoms.get(symptom));
+					writer.newLine();
+				}
+				writer.close();
+			}
+			catch (IOException e) {
+				System.out.println("Beurre " + e.getMessage()) ;
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Beurre");
+		}
+	}
 }
 
 
